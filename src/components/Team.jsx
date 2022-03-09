9219f3c45df657e1bd3dba620596ef00e8fb5e04
sena <paxlve@outlook.com>
2022-03-10 02:55:23 +0300

    responsive fixes

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -11,7 +11,7 @@ export const Team = (props) => {
                 <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
                   <div className='thumbnail'>
                     {' '}
-                    <img src={d.img} alt='...' className='team-img' />
+                    <img src={d.img} alt='...' className='team-img img-responsive' />
                     <div className='caption'>
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
